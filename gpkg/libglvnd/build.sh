f9910a88fe791a16399db23200c5953822d64efa
MOZE <165124056+moze30@users.noreply.github.com>
2026-03-01 20:12:04 +0800

    修复脚本确保编译正常进行
    修改后，让Meson 将完全忽略 tests 目录，从而避免因缺少链接库导致的错误。

--- gpkg/libglvnd/build.sh
+++ gpkg/libglvnd/build.sh
@@ -10,24 +10,34 @@ TERMUX_PKG_DEPENDS="libxext-glibc"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto-glibc"
 TERMUX_PKG_RECOMMENDS="mesa-glibc"
 
+termux_step_configure() {
+    # 注释掉顶层 meson.build 中的 subdir('tests')，避免尝试进入 tests 目录
+    local meson_file="$TERMUX_PKG_SRCDIR/meson.build"
+    if [ -f "$meson_file" ]; then
+        sed -i "s/subdir('tests')/#subdir('tests')/g" "$meson_file"
+    fi
+
+    termux_step_configure_meson
+}
+
 termux_step_install_license() {
-	install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
-		$TERMUX_PKG_BUILDER_DIR/LICENSE
+    install -Dm600 -t $TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME \
+        $TERMUX_PKG_BUILDER_DIR/LICENSE
 }
 
 termux_step_post_massage() {
-	# A bunch of programs in the wild assume that the name of OpenGL shared
-	# library is `libGL.so.1` and try to dlopen(3) it. In fact `sdl2` does
-	# this. Also `libEGL.so` and some others need SOVERSION suffix to avoid
-	# conflict with system ones. So let's check if SONAME is properly set.
-	local n
-	for n in GL EGL GLESv1_CM GLESv2; do
-		local f="glibc/lib/lib${n}.so"
-		if [ ! -e "${f}" ]; then
-			termux_error_exit "Shared library ${f} does not exist."
-		fi
-		if ! readelf -d "${f}" | grep -q '(SONAME).*\[lib'"${n}"'\.so\.'; then
-			termux_error_exit "SONAME for ${f} is not properly set."
-		fi
-	done
+    # A bunch of programs in the wild assume that the name of OpenGL shared
+    # library is `libGL.so.1` and try to dlopen(3) it. In fact `sdl2` does
+    # this. Also `libEGL.so` and some others need SOVERSION suffix to avoid
+    # conflict with system ones. So let's check if SONAME is properly set.
+    local n
+    for n in GL EGL GLESv1_CM GLESv2; do
+        local f="glibc/lib/lib${n}.so"
+        if [ ! -e "${f}" ]; then
+            termux_error_exit "Shared library ${f} does not exist."
+        fi
+        if ! readelf -d "${f}" | grep -q '(SONAME).*\[lib'"${n}"'\.so\.'; then
+            termux_error_exit "SONAME for ${f} is not properly set."
+        fi
+    done
 }
